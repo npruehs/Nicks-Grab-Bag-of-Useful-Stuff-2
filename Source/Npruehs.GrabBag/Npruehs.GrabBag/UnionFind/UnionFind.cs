863e7483b4e0c71a6a3bf156b57bd26bc3fcc182
Nick Pruehs <dev@npruehs.de>
2013-03-19 14:15:44 +0100

    CHANGED: Split up the union-find structure and its dictionary wrapper.

--- Source/Npruehs.GrabBag/Npruehs.GrabBag/UnionFind/UnionFind.cs
+++ Source/Npruehs.GrabBag/Npruehs.GrabBag/UnionFind/UnionFind.cs
@@ -10,8 +10,10 @@ namespace Npruehs.GrabBag.UnionFind
     using System.Collections.Generic;
 
     /// <summary>
-    /// Implementation of <i>Robert Endre Tarjan</i>'s union-find structure,
-    /// using union with size and find with path compression.
+    /// Wraps <i>Robert Endre Tarjan</i>'s union-find structure,
+    /// using union with size and find with path compression, by mapping
+    /// elements to union-find nodes with a dictionary. Use
+    /// <see cref="UnionFindStructure{T}"/> for handling the nodes yourself.
     /// </summary>
     /// <typeparam name="T">
     /// Type of the elements of the union-find universe.
@@ -25,6 +27,11 @@ namespace Npruehs.GrabBag.UnionFind
         /// Maps elements of the universe to their respective union-find nodes.
         /// </summary>
         private readonly Dictionary<T, UnionFindNode<T>> nodes = new Dictionary<T, UnionFindNode<T>>();
+
+        /// <summary>
+        /// Union-find structure handling unions and finds.
+        /// </summary>
+        private readonly UnionFindStructure<T> unionFind = new UnionFindStructure<T>();
 
         #endregion
 
@@ -53,41 +60,8 @@ namespace Npruehs.GrabBag.UnionFind
                         "Unknown item: {0}. Use MakeSet for adding new elements to this union-find universe.", item));
             }
 
-            var canonical = this.FindNode(node);
+            var canonical = this.unionFind.Find(node);
             return canonical.Item;
-        }
-
-        /// <summary>
-        /// Returns the canonical element of the set containing v.
-        /// The question "Are v and w in the same set?" can be reduced to Find(v) == Find(w).
-        /// </summary>
-        /// <param name="v">
-        /// Node to get the canonical element of.
-        /// </param>
-        /// <returns>
-        /// Canonical element of the set containing v.
-        /// </returns>
-        public UnionFindNode<T> FindNode(UnionFindNode<T> v)
-        {
-            // Find root note.
-            var root = v;
-
-            while (root.Parent != null)
-            {
-                root = root.Parent;
-            }
-
-            // Path compression.
-            var current = v;
-
-            while (current != root)
-            {
-                var next = current.Parent;
-                current.Parent = root;
-                current = next;
-            }
-
-            return root;
         }
 
         /// <summary>
@@ -98,7 +72,7 @@ namespace Npruehs.GrabBag.UnionFind
         /// </param>
         public void MakeSet(T item)
         {
-            var set = this.MakeSetNode(item);
+            var set = this.unionFind.MakeSet(item);
 
             if (!this.nodes.ContainsKey(item))
             {
@@ -111,20 +85,6 @@ namespace Npruehs.GrabBag.UnionFind
                         "This union-find universe already contains the element {0}. Elements in union-find structures are unique.", 
                         item));
             }
-        }
-
-        /// <summary>
-        /// Adds a new singleton set containing the passed item.
-        /// </summary>
-        /// <param name="item">
-        /// Item to add.
-        /// </param>
-        /// <returns>
-        /// Container holding the passed item.
-        /// </returns>
-        public UnionFindNode<T> MakeSetNode(T item)
-        {
-            return new UnionFindNode<T>(item);
         }
 
         /// <summary>
@@ -152,46 +112,13 @@ namespace Npruehs.GrabBag.UnionFind
 
             if (this.nodes.TryGetValue(second, out nodeSecond))
             {
-                this.UnionNodes(nodeFirst, nodeSecond);
+                this.unionFind.Union(nodeFirst, nodeSecond);
             }
             else
             {
                 throw new InvalidOperationException(
                     string.Format(
                         "Unknown item: {0}. Use MakeSet for adding new elements to this union-find universe.", second));
-            }
-        }
-
-        /// <summary>
-        /// Merges the sets containing the canonical elements
-        /// <paramref name="v"/> and <paramref name="w"/>.
-        /// </summary>
-        /// <param name="v">
-        /// An element in the first set to merge.
-        /// </param>
-        /// <param name="w">
-        /// An element in the second set to merge.
-        /// </param>
-        public void UnionNodes(UnionFindNode<T> v, UnionFindNode<T> w)
-        {
-            var rootV = this.FindNode(v);
-            var rootW = this.FindNode(w);
-
-            if (v == w)
-            {
-                return;
-            }
-
-            // Union with size: Make the root of the smaller tree a child of the root of the larger one.
-            if (rootV.SubtreeSize < rootW.SubtreeSize)
-            {
-                rootV.Parent = rootW;
-                rootW.SubtreeSize += rootV.SubtreeSize;
-            }
-            else
-            {
-                rootW.Parent = rootV;
-                rootV.SubtreeSize += rootW.SubtreeSize;
             }
         }
 
---
+++ Source/Npruehs.GrabBag/Npruehs.GrabBag/UnionFind/UnionFindStructure.cs
@@ -0,0 +1,105 @@
+﻿// --------------------------------------------------------------------------------
+// <copyright file="UnionFindStructure.cs" company="Nick Pruehs">
+//   Copyright 2013 Nick Pruehs.
+// </copyright>
+// --------------------------------------------------------------------------------
+
+namespace Npruehs.GrabBag.UnionFind
+{
+    using System;
+
+    /// <summary>
+    /// Implementation of <i>Robert Endre Tarjan</i>'s union-find structure,
+    /// using union with size and find with path compression.
+    /// </summary>
+    /// <typeparam name="T">
+    /// Type of the elements of the union-find universe.
+    /// </typeparam>
+    [CLSCompliant(true)]
+    public class UnionFindStructure<T>
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Returns the canonical element of the set containing v.
+        /// The question "Are v and w in the same set?" can be reduced to Find(v) == Find(w).
+        /// </summary>
+        /// <param name="v">
+        /// Node to get the canonical element of.
+        /// </param>
+        /// <returns>
+        /// Canonical element of the set containing v.
+        /// </returns>
+        public UnionFindNode<T> Find(UnionFindNode<T> v)
+        {
+            // Find root note.
+            var root = v;
+
+            while (root.Parent != null)
+            {
+                root = root.Parent;
+            }
+
+            // Path compression.
+            var current = v;
+
+            while (current != root)
+            {
+                var next = current.Parent;
+                current.Parent = root;
+                current = next;
+            }
+
+            return root;
+        }
+
+        /// <summary>
+        /// Adds a new singleton set containing the passed item.
+        /// </summary>
+        /// <param name="item">
+        /// Item to add.
+        /// </param>
+        /// <returns>
+        /// Container holding the passed item.
+        /// </returns>
+        public UnionFindNode<T> MakeSet(T item)
+        {
+            return new UnionFindNode<T>(item);
+        }
+
+        /// <summary>
+        /// Merges the sets containing the canonical elements
+        /// <paramref name="v"/> and <paramref name="w"/>.
+        /// </summary>
+        /// <param name="v">
+        /// An element in the first set to merge.
+        /// </param>
+        /// <param name="w">
+        /// An element in the second set to merge.
+        /// </param>
+        public void Union(UnionFindNode<T> v, UnionFindNode<T> w)
+        {
+            var rootV = this.Find(v);
+            var rootW = this.Find(w);
+
+            if (v == w)
+            {
+                return;
+            }
+
+            // Union with size: Make the root of the smaller tree a child of the root of the larger one.
+            if (rootV.SubtreeSize < rootW.SubtreeSize)
+            {
+                rootV.Parent = rootW;
+                rootW.SubtreeSize += rootV.SubtreeSize;
+            }
+            else
+            {
+                rootW.Parent = rootV;
+                rootV.SubtreeSize += rootW.SubtreeSize;
+            }
+        }
+
+        #endregion
+    }
+}
